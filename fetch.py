b3b48ec7b9bbd8a32a20848e59a26e892e4ef082
Dipan Mehta <dipanm@vubites.com>
2016-02-14 00:10:32 +0530

    Fixed a critical bug in stream_map of caption. Though not fully tested for all type of streams.

--- fetch.py
+++ fetch.py
@@ -39,7 +39,7 @@ global logging_level
 global log_dir 
 global stream_log_path
 
-logging_level = logging.INFO 
+logging_level = logging.DEBUG
 log_dir = "./logs" 
 
 def get_watch_page(vid):
@@ -90,7 +90,7 @@ def parse_stream_map(argstr):
 	arg_list = json.loads(argstr)
 	encoded_map = arg_list['args']['url_encoded_fmt_stream_map'].split(",") 
 	encoded_map_adp = arg_list['args']['adaptive_fmts'].split(",")
-
+	
 	res_index = { 'small': '240', 'medium': '360', 'high': '480', 'hd720': '720', '1440p': '1440', '1080p': '1080'  } 
 
 	logr.info("\nMedia stream options: Std: %d ADP: %d",len(encoded_map),len(encoded_map_adp))
@@ -156,7 +156,27 @@ def parse_stream_map(argstr):
 	adp_stream_map_v = sorted(adp_stream_map_v, key= lambda k: int(k['res']), reverse=True) 
 	adp_stream_map_a = sorted(adp_stream_map_a, key= lambda k: int(k['bitrate']), reverse=True) 
 
-	return { 'std': fmt_stream_map, 'adp_v': adp_stream_map_v, 'adp_a': adp_stream_map_a }
+	caption_map = list() 
+	if(arg_list['args'].has_key('caption_tracks')): 
+		caption_str = arg_list['args']['caption_tracks'].split(",") 
+		for cmap in caption_str:
+			cap_map = dict() 
+			cap = cmap.split("&")
+			for c in cap: 
+				c_attrib = c.split("=") 
+				if(c_attrib[0] == 'u'): 
+					c_attrib[0] = 'url'
+				if(c_attrib[0] == 'lc'): 
+					c_attrib[0] = 'lang'
+				if(c_attrib[0] == 'n'): 
+					c_attrib[0] = 'name'
+				c_attrib[1] = urllib.unquote(c_attrib[1]).decode('utf8') 
+				c_attrib[1] = urllib.unquote(c_attrib[1]).decode('utf8') 
+				cap_map.update({str(c_attrib[0]):str(c_attrib[1])})
+				cap_map.update({'media':'caption','fmt':'srt'}) 
+			caption_map.append(cap_map)
+
+	return { 'std': fmt_stream_map, 'adp_v': adp_stream_map_v, 'adp_a': adp_stream_map_a, 'caption': caption_map }
 
 def print_smap_detailed(map_name,smap):
 	logr = logging.getLogger(vid) 
@@ -166,7 +186,7 @@ def print_smap_detailed(map_name,smap):
 	for s in smap:
 		logr.debug("%d -----------------------------------",i) 
 		for key in sorted(s):
-			logr.debug("%s,:   %s",key,s[key]) 
+			logr.debug("%s:   %s",key,s[key]) 
 		i += 1
 		
 def print_smap_abridged(map_name,smap):
@@ -180,17 +200,21 @@ def print_smap_abridged(map_name,smap):
 			logr.info("%s (%sp) [%s]",s['quality_label'],str(s['size']),s['type']) 
 		if s['media'] == "audio":
 			logr.info("%s kbps, [%s]",int(s['bitrate'])/1024,s['type']) 
+		if s['media'] == "caption":
+			logr.info("%s %s, [%s]",s['lang'],s['fmt'],s['name']) 
 
 def print_stream_map_detailed(stream_map):
 	print_smap_detailed("Standard",stream_map['std'])
 	print_smap_detailed("ADP Video",stream_map['adp_v'])
 	print_smap_detailed("ADP Audio",stream_map['adp_a'])
+	print_smap_detailed("Captions",stream_map['caption'])
 		
 	
 def print_stream_map_abridged(stream_map):
 	print_smap_abridged("Standard",stream_map['std'])
 	print_smap_abridged("ADP Video",stream_map['adp_v'])
 	print_smap_abridged("ADP Audio",stream_map['adp_a'])
+	print_smap_abridged("Captions",stream_map['caption'])
 
 
 def dlProgress(count, blockSize, totalSize):
@@ -205,9 +229,31 @@ def dlProgress(count, blockSize, totalSize):
 	disp_interval = 5
 	if(count == 0):
 		logr.info("Filesize %d Bytes",totalSize) 
-	if((count % (count_p*disp_interval) == 0) or (percent == 100)):
-		tnow = datetime.datetime.now()
-		logr.info("%s : %d%% - %d of %d MB",str(tnow),percent,(count*blockSize)/1000/1000,(totalSize/1000/1000))
+	if(totalSize > 1000*1000): 
+		if((count % (count_p*disp_interval) == 0) or (percent == 100)):
+			tnow = datetime.datetime.now()
+			logr.info("%s : %d%% - %d of %d MB",str(tnow),percent,(count*blockSize)/1000/1000,(totalSize/1000/1000))
+
+def select_captions(caption_map):
+	logr = logging.getLogger(vid) 
+
+	logr.info("in select caption")
+
+	if(len(caption_map) > 0):
+		for cmap in caption_map:
+			if 'en' not in cmap['lang']:
+				logr.info("cmp %s not needed",str(cmap))
+	if (len(caption_map) > 1):
+		for cmap in caption_map:
+			if 'auto' in cmap['lang']:
+				del caption_map[cmap] 
+			if(len(caption_map) == 1):
+				return caption_map[0]
+		return caption_map[0]
+	elif(len(caption_map) == 1):
+		return caption_map[0]
+	else:
+		return {}  		# Shouldn't come here. 
 
 def select_best_stream(stream_map):
 	max_res_std = int(stream_map['std'][0]['res'])
@@ -215,10 +261,14 @@ def select_best_stream(stream_map):
 
 	select_map = list(); 
 	if(max_res_adp > max_res_std):
-		select_map.append(stream_map['adp_v'][0]);
-		select_map.append(stream_map['adp_a'][0]);
+		select_map.append(stream_map['adp_v'][0])
+		select_map.append(stream_map['adp_a'][0])
 	else:
-		select_map.append(stream_map['std'][0]);
+		select_map.append(stream_map['std'][0])
+
+	caption_map = select_captions(stream_map['caption'])
+	if(caption_map):
+		select_map.append(caption_map)
 
 	#select_map.append(stream_map['std'][0]);
 	return select_map 
@@ -228,6 +278,8 @@ def download_streams(page, select_map,folder):
 
 	title = page['title'] 
 	uid = page['vid'] 
+	out_fmt = "mp4"
+ 
 
 	separated = 1; 	# Assume sepeated content by default. If not, no need to merge 
 	temp_files = dict(); 
@@ -250,7 +302,8 @@ def download_streams(page, select_map,folder):
 		logr.info("%sTime taken %s\n---------------------------------",msg,str(t1-t0)) 
 	
 	if(separated == 1):
-		outfile = folder.rstrip('/')+"/"+str(title)+"_-_"+str(uid)+"."+str(smap['fmt'])
+		#outfile = folder.rstrip('/')+"/"+str(title)+"_-_"+str(uid)+"."+str(smap['fmt'])
+		outfile = folder.rstrip('/')+"/"+str(title)+"_-_"+str(uid)+"."+out_fmt 
 		#stream_log_path = log_dir.rstrip('/')+"/"+vid+".log"
 		slog_path = "./ff.log" 
 		cmd = "ffmpeg -y -i "+temp_files['video']+" -i "+temp_files['audio']+" -acodec copy \""+outfile+"\" >ff.log"
@@ -293,8 +346,8 @@ def download_item(vid,folder):
 	argstr =  get_stream_map_serial(watch_page['tree'])
 	stream_map = parse_stream_map(argstr)
 
-	print_stream_map_abridged(stream_map)
 	print_stream_map_detailed(stream_map)
+	print_stream_map_abridged(stream_map)
 
 	select_map = select_best_stream(stream_map) 
 	print_smap_abridged("Selected",select_map)
