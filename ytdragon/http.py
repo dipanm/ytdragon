3cac58e13b24568df71c9de16c8d7d83c0a44fea
dipanm <dipanmehta@gmail.com>
2019-05-08 16:55:14 +0530

    cleaned up dlProgress and get_stream

--- ytdragon/http.py
+++ ytdragon/http.py
@@ -4,30 +4,22 @@ import datetime
 import socket
 import urllib
 import requests
+from ytdragon.utils import print_pretty
 
-def dlProgress(count, blockSize, totalSize):
-        #global vid
-        #logm = logging.getLogger()
-        #logr = logging.getLogger(vid)
+def dlProgress(vid, count, actual_size, totalSize,last=False):
+        logm = logging.getLogger()
+        logr = logging.getLogger(vid)
+
+        percent = int(actual_size*100/totalSize) if(totalSize > 0) else 0
 
         counter_str = [ "|","/","-","\\"]
-        cstr = counter_str[(int(count/100))%4]
+        cstr = counter_str[(int(count))%4]
 
-        if(totalSize > 0):
-                percent = int(count*blockSize*100/totalSize)
-        else:
-                percent = 0
-
-        count_p = totalSize/blockSize/100+1
-        disp_interval = 5
-        """
         if(count == 0):
                 logr.debug("Filesize %d Bytes",totalSize)
-        if(totalSize > 1000*1000):
-                if((count % (count_p*disp_interval) == 0) or (percent == 100)):
-                        tnow = datetime.datetime.now()
-                        logr.debug("%s : %d%% - %d of %d MB",str(tnow),percent,(count*blockSize)/1000/1000,(totalSize/1000/1000))
-        """
+        if( (actual_size == totalSize) or last):
+                tnow = datetime.datetime.now()
+                logr.debug("%s : %d%% - %d of %d MB",str(tnow),percent,(actual_size)/1000/1000,(totalSize/1000/1000))
 
         sys.stdout.write("\r\tDownload progress: %s %d%% of %d MB " % (cstr,percent, totalSize/1000/1000) )
         sys.stdout.flush()
@@ -36,6 +28,7 @@ def dlProgress(count, blockSize, totalSize):
 class DlHttp:
 
     def __init__(self,vid):
+        self.vid = vid
         self.logr = logging.getLogger(vid)
 
     def get_stream(self,smap_string,url,filename):
@@ -46,12 +39,13 @@ class DlHttp:
         socket.setdefaulttimeout(120)
         #print("\n--------\n",url,"\n--------\n")
 
+        # Begin Request
         r = requests.get(url, stream=True)
-        filesize = int(r.headers.get('content-length'))
-        self.logr.info("Status:{} Content-Type:{} TotalSize:{}".format(r.status_code,r.headers['Content-Type'],filesize))
+        filesize = int(r.headers['Content-Length'])
+        print_pretty(self.logr,"Headers:",r.headers)
 
         # do this only if status = 200OK
-        received = 0
+        count = 0
         actual_size = 0
         block_size = 1024*1024
         with open(filename, 'wb') as fd:
@@ -59,13 +53,12 @@ class DlHttp:
                 if chunk:
                     actual_size += len(chunk)
                     fd.write(chunk)
-                    received += 1
-                    dlProgress(received, block_size, filesize)
+                    count += 1
+                    dlProgress(self.vid,count,actual_size,filesize)
                 else:
                     break
                     self.logr.info("\nFile download complete size:received {}\n".format(actual_size))
 
-        #fname, msg = urllib.request.urlretrieve(url,filename,reporthook=dlProgress)
         t1 = datetime.datetime.now()
         sys.stdout.write("\r")
         sys.stdout.flush()
